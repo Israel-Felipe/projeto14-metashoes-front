14073cb5bafb09908c6474a5e4e2a5c0b337fac5
elias <eliasmanica@poli.ufrj.br>
2022-09-14 15:03:25 -0300

    criando layout top bar e pagina

---
+++ src/components/TopBar/styles.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const Bar = styled.div`
+  background-color: #171717;
+  height: 80px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ContainerLogo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 5%;
+`;
+
+export const TittleLogo = styled.h1`
+  font-family: "Hermeneus One";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 35px;
+  margin-right: 3px;
+  color: #ffffff;
+  text-shadow: 0px 4px 4px #74e6e9;
+  -webkit-text-stroke: 0.5px #b543b9;
+`;
+
+export const SubTittleLogo = styled.h2`
+  font-family: "Hermeneus One";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  color: #fe5fff;
+  -webkit-text-stroke: 0.5px #b543b9;
+  text-shadow: 0px 4px 4px #000000;
+`;
+
+export const ContainerRoutes = styled.div`
+  margin-right: 5%;
+  display: flex;
+  align-items: center;
+`;
+
+export const TextRoutes = styled.h3`
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  color: #ffffff;
+  -webkit-text-stroke: 0.5px #b543b9;
+  text-shadow: 0px 4px 4px #171717;
+  margin-right: 30px;
+  cursor: pointer;
+`;
+
+export const Icon = styled.div`
+  color: white;
+  font-size: 30px;
+  margin-right: 30px;
+`;
---
+++ src/screens/App.js
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "../assets/GlobalStyles";
+
+import ProductPage from "./ProductPage/ProductPage";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <ProductPage />
+    </>
+  );
+}
